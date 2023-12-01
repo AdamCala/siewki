3c53fba6304999702fc7c7e3a79a643d67512fe6
Adam Cała <adam.cala31@gmail.com>
2023-12-01 13:53:35 +0100

    smoother animations

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,9 @@
 import Logo from "../components/icons/logo";
-import { Link, redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/index.module.scss";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import { backInOut } from "framer-motion/dom";
 
 const Root = () => {
   const navigate = useNavigate();
@@ -22,23 +23,23 @@ const Root = () => {
   };
 
   return (
-    <div className="w-full h-full flex flex-row">
+    <div className={`w-full h-full flex flex-row ${styles.main}`}>
       <div className={`absolute w-screen h-screen z-0 ${styles.right}`}></div>
       <AnimatePresence mode="wait">
         <motion.div
           key={animate}
           initial={{}}
           exit={{ width: "100%" }}
-          transition={{ duration: 1, delay: 1 }}
+          transition={{ duration: 1, delay: 1, ease: backInOut }}
           className={`${styles.left} w-2/3 h-screen flex flex-col justify-center z-10`}
         >
           <motion.p
             key={animate}
-            exit={{ y: -1000, scale: 0, type: "tween" }}
-            transition={{ duration: 1 }}
+            exit={{ y: -1000, scale: [1, 0.8, 0.6, 0], type: "tween" }}
+            transition={{ duration: 1.2, ease: backInOut }}
             className={`${styles.text} tracking-widest text-9xl font-black italic z-10`}
           >
-            {isVisible && <p>SIEWKI</p>}
+            {isVisible && <motion.p>SIEWKI</motion.p>}
           </motion.p>
 
           <div className="w-full h-3/6 z-10"></div>
@@ -50,11 +51,19 @@ const Root = () => {
         <AnimatePresence mode="popLayout">
           <motion.div
             key={animate}
-            exit={{ y: -1000, scale: 0, type: "tween" }}
-            transition={{ duration: 1 }}
+            exit={{ y: -1000, scale: [1, 0.8, 0.6, 0], type: "tween" }}
+            transition={{ duration: 1, ease: backInOut }}
             className="w-2/3 flex justify-center items-center z-10"
           >
-            {isVisible && <Logo onClick={handleLogoClick}></Logo>}
+            {isVisible && (
+              <motion.div
+                transition={{ duration: 1.2, ease: backInOut }}
+                whileHover={{ scale: 1.05 }}
+                className="w-fit h-fit cursor-pointer"
+              >
+                <Logo onClick={handleLogoClick}></Logo>
+              </motion.div>
+            )}
           </motion.div>
         </AnimatePresence>
       </div>
