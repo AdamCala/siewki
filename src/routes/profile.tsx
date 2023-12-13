a4a2aed442f52026488dbd6e15111770ab7b8a88
Adam Cała <adam.cala31@gmail.com>
2023-12-13 13:55:58 +0100

    profile picture display

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,22 +1,33 @@
-import DesktopBackground from "../components/assets/desktopBackground";
 import Logout from "../components/icons/logout";
 import Settings from "../components/icons/settings";
+import { useAppSelector } from "../hooks/storeHook";
 import styles from "../styles/profile/index.module.scss";
 
 const profile = () => {
+  const { user } = useAppSelector((state) => state.auth);
   return (
     <>
-      <DesktopBackground
-        className={`${styles.background_svg} absolute bottom-0`}
-      />
       <div className={`${styles.main} w-screen h-screen`}>
-        <div className={`${styles.circle} rounded-full`}>
-          <div className={`${styles.circle_md} rounded-full`}>
-            <Logout className={`${styles.icon}`} />
-          </div>
-          <div className={`${styles.circle_sm} rounded-full`}>
-            <Settings className={`${styles.icon}`} />
-          </div>
+        <div
+          className={`${styles.circle} rounded-full flex justify-center items-center`}
+        >
+          {user?.photoUrl ? (
+            <img
+              className="w-4/5 rounded-full"
+              src={user.photoUrl}
+              alt="avatar"
+            />
+          ) : (
+            <div className="w-4/5 h-4/5 rounded-full">
+              {user?.email[0].toUpperCase()}
+            </div>
+          )}
+        </div>
+        <div className={`${styles.circle_md} rounded-full`}>
+          <Logout className={`${styles.icon}`} />
+        </div>
+        <div className={`${styles.circle_sm} rounded-full`}>
+          <Settings className={`${styles.icon}`} />
         </div>
       </div>
     </>
