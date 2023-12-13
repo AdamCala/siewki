821bf0fb0670b99b2a85ad15c073af06223e592c
Adam Cała <adam.cala31@gmail.com>
2023-12-13 16:56:26 +0100

    added settings page

---
+++ src/components/settingsPage.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import styles from "../styles/components/settingsPage.module.scss";
+
+interface settingsProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const settingsPage: FC<settingsProps> = (props) => {
+  const { isOpen, onClose } = props;
+  return (
+    <div
+      className={`fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-center items-center bg-black bg-opacity-50`}
+      style={{ transform: `translateY(${isOpen ? "0%" : "-100%"})` }}
+    >
+      <div className={`${styles.main}`}>
+        <div className={`${styles.line} `}>
+          <div />
+          <button className="my-5" onClick={onClose}>
+            Close
+          </button>
+          <div />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default settingsPage;
--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Logout from "../components/icons/logout";
 import Settings from "../components/icons/settings";
-import { useAppSelector } from "../hooks/storeHook";
+import { useAppDispatch, useAppSelector } from "../hooks/storeHook";
 import styles from "../styles/profile/index.module.scss";
+
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { logout } from "../features/authSlice";
+import SettingsPage from "../components/settingsPage";
 
 const profile = () => {
   const { user } = useAppSelector((state) => state.auth);
+  const [openSettings, setOpenSettings] = useState(false);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    dispatch(logout());
+  };
+
+  useEffect(() => {
+    // if (Boolean(!user)) {
+    //     navigate('/auth');
+    // }
+  }, [navigate, user]);
+
   return (
     <>
+      <SettingsPage
+        isOpen={openSettings}
+        onClose={() => setOpenSettings(false)}
+      />
       <div className={`${styles.main} w-screen h-screen`}>
         <div
           className={`${styles.circle} rounded-full flex justify-center items-center`}
@@ -23,10 +50,16 @@ const profile = () => {
             </div>
           )}
         </div>
-        <div className={`${styles.circle_md} rounded-full`}>
+        <div
+          onClick={handleLogout}
+          className={`${styles.circle_md} rounded-full cursor-pointer`}
+        >
           <Logout className={`${styles.icon}`} />
         </div>
-        <div className={`${styles.circle_sm} rounded-full`}>
+        <div
+          onClick={() => setOpenSettings(true)}
+          className={`${styles.circle_sm} rounded-full cursor-pointer`}
+        >
           <Settings className={`${styles.icon}`} />
         </div>
       </div>
