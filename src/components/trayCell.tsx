df2a1915b2cc9681498f06bce95d6e1348163fdd
Adam Cała <adam.cala31@gmail.com>
2024-03-04 13:35:23 +0100

    redirecting to trays

--- src/components/trayCell.tsx
+++ src/components/trayCell.tsx
@@ -1,10 +1,16 @@
 import { trayResult } from "../routes/profile";
 import styles from "../styles/components/trayCell.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const trayCell = (props: trayResult) => {
   const { id, cols, rows, name } = props;
+  const navigate = useNavigate();
+
+  function handleOnClick() {
+    navigate(`/tray/${id}`);
+  }
   return (
-    <div key={id} className={styles.main}>
+    <div key={id} className={styles.main} onClick={handleOnClick}>
       <p>{name}</p>
       <p>
         {rows}x{cols}
